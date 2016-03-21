18909d081bcb2263eecdfdd54f679a90c527a642
VarelaCristianFacundo <cfacundovarela@yahoo.com>
2016-03-21 01:00:44 -0300

    Sistema de Login

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+session_start();
+?>
+<html>
+	<head>
+		<title>Proyecto Programación</title>
+		<link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.min.css">
+	</head>
+	<body>
+	<?php include "php/navbar.php"; ?>
+	<div class="container">
+	<div class="row">
+	<div class="col-md-12">
+			<ul type="none">
+			</ul>
+
+	</div>
+	</div>
+	</div>
+	</body>
+</html>
